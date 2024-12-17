62d6365996f7ac89570568f5bee00fc2cf5d1beb
pcScuola <filippo.boschi.008@gmail.com>
2024-12-17 12:32:45 +0100

    es 3ù

--- Esercizio3.asm
+++ Esercizio3.asm
@@ -4,6 +4,7 @@ data segment
     ; add your data here!
     Num1 DW 10h
     Num2 DW 20h
+    Num3 DW 30h
     Ris  DW  0h
     pkey db "press any key...$"
 ends
@@ -19,6 +20,9 @@ MOV DS,AX
 MOV AX,Num1    
 MOV BX,Num2  
 ADD AX,BX
+MOV CX,Num3
+ADD AX,CX
+MOV Ris,AX
 
 
 ends
