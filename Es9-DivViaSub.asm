1563dc6e75385ceb89f311c32f40b83456cfe3c8
pcScuola <filippo.boschi.008@gmail.com>
2025-02-04 12:30:18 +0100

    sistemazione di es 9

--- Es9-DivViaSub.asm
+++ Es9-DivViaSub.asm
@@ -2,7 +2,7 @@
 
 data segment
     Num1 dw 503h
-    Dden dw 5h
+    Dden dw 0h
     Quoz dw 0h
     Rest dw 0h
     pkey db "press any key...$"
@@ -20,6 +20,9 @@ start:
     mov es, ax
 
     mov ax,Num1
+    mov bx,Dden
+    cmp bx,0h
+    je fine_su_zero
 inizio_ciclo:
     cmp ax,Dden
     jl fine
@@ -28,7 +31,9 @@ inizio_ciclo:
     jmp inizio_ciclo
     
 fine: 
-     mov Rest,ax   
+     mov Rest,ax
+fine_su_zero:
+       
 ends
 
 end start ; set entry point and stop the assembler.
