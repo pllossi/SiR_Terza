f53a251613a7261d82784c14306c163020b91f8b
pllossi <115420435+pllossi@users.noreply.github.com>
2025-02-25 22:26:08 +0100

    Calcolatrice

--- Calcolatrice.asm
+++ Calcolatrice.asm
@@ -1,13 +1,16 @@
-; multi-segment executable file template.
+ ; multi-segment executable file template.
 
 data segment
     Msg             db "Inserisci un numero terminando premendo invio: $"
     MsgErr          db "Errore:Inserito input non valido $"
+    MsgSegn         db "Inserisci + per sommare, - per sottrarre, * per moltiplicare, / per dividere $"
+    MsgRis          db "Il risultato e': $" ,
     Cifra           dw 0
     NumIns1         dw 0 
     NumIns2         dw 0
     Segn            dw 0
-    pkey db "press any key...$"
+    Ris             dw 0
+    pkey            db "press any key...$"
 ends
 
 stack segment
@@ -34,8 +37,11 @@ start:
     mov Cifra,0               
     call LeggiNumero2
     
+    lea dx,MsgSegn
+    call LeggiSegno
+    call NewLine
     
-    
+    call ScriviRisultato   
     mov ax, 4c00h ; exit to operating system.
     int 21h    
 ends
@@ -45,7 +51,7 @@ LeggiNumero1 proc near
 Leggi1:
     mov ah,01h
     int 21h
-    cmp ah,0DH
+    cmp al,0DH
     je FineInserimento1
     cmp al,30h
     jl Errore1
@@ -64,9 +70,8 @@ Leggi1:
         
 Errore1:         
     lea dx,MsgErr
-    mov ah,09h
-    int 21h
-    jmp Leggi
+    call Scrivi
+    jmp Leggi1
     
     
 FineInserimento1:
@@ -86,7 +91,7 @@ LeggiNumero2 proc near
 Leggi2:
     mov ah,01h
     int 21h
-    cmp ah,0DH
+    cmp al,0DH
     je FineInserimento2
     cmp al,30h
     jl Errore2
@@ -105,9 +110,8 @@ Leggi2:
         
 Errore2:         
     lea dx,MsgErr
-    mov ah,09h
-    int 21h
-    jmp Leggi
+    call Scrivi
+    jmp Leggi2
     
     
 FineInserimento2:
@@ -116,13 +120,75 @@ FineInserimento2:
 
 endp
 
-Scrivi proc near
-    mov ah,09h
+LeggiSegno proc near
+    mov bx,0
+LeggiSegn:
+    mov ah,01h
     int 21h
+    cmp al,2Ah
+    je InserSegn
+    cmp al,2Bh
+    je InserSegn
+    cmp al,2Dh
+    je InserSegn
+    cmp al,2Fh
+    je InserSegn
+    jmp ErroreSegn
+        
+InserSegn:
+    mov Segn,bx 
+    jmp FineInserimentoSegno
+ErroreSegn:         
+    lea dx,MsgErr
+    call Scrivi
+    jmp LeggiSegn
+FineInserimentoSegno:
+    ret
+
+endp
+
+
+Calcola proc near
+    cmp Segn,2Ah
+    je Molt
+    cmp Segn,2Bh
+    je Somma
+    cmp Segn,2Dh
+    je Sott
+    cmp Segn,2Fh
+    je Divs
+Somma:
+    mov ax,NumIns1
+    add ax,NumIns2
+    jmp FineCalc
+Molt:
+    mov ax,NumIns1
+    mov bx,NumIns2
+    mul bx
+    jmp FineCalc
+Sott:
+    mov ax,NumIns1
+    sbb ax,NumIns2
+    jmp FineCalc
+Divs:
+    mov ax,NumIns1
+    mov bx,NumIns2
+    div bx
+    jmp FineCalc
+FineCalc:
+    mov Ris,ax
     ret
 endp
 
-NewLine proc near 
+ScriviRisultato proc near
+    lea dx,MsgRis
+    call Scrivi
+    mov dx,Ris
+    call Scrivi
+    ret
+endp  
+
+NewLine proc near
     MOV DL,0Ah 
     MOV AH,02
     INT 21h  
@@ -130,36 +196,4 @@ NewLine proc near
     INT 21h
     ret
 endp
-
-LeggiSegno proc near
-    mov bx,0
-LeggiSegn:
-    mov ah,01h
-    int 21h
-    cmp ah,0DH
-    je FineInserimento
-    cmp al,30h
-    jl Errore
-    cmp al,39h
-    jg Errore
-        
-Errore:         
-    lea dx,MsgErr
-    mov ah,09h
-    int 21h
-    jmp Leggi
-    
-    
-FineInserimento:
-    mov NumIns2,bx
-    ret
-
-endp
-
-Scrivi proc near
-    mov ah,09h
-    int 21h
-    ret
-endp
-
 end start ; set entry point and stop the assembler.
